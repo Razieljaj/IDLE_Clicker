d381f6138a5c554d2e7eec3f1f7d0720e8f9aa5c
Ewen <ewen.rigaud@gmail.com>
2025-02-26 17:23:57 +0100

    bug

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/home")
-def hello_world():
-    score = 20
-    return render_template("index.html"),score
+@app.route("/home",methods=['GET', 'POST'])
+def home():
+    score = 0
+    if request.method == "POST":
+        if request.form['click'] == 3:
+	        score = score + 1
+    return render_template("index.html",score = score)
 
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
